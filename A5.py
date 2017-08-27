d424aa47805520667acb5344fe4ee768d47574b0
qydchen <qydchen@gmail.com>
2017-08-27 05:10:29 -0400

    do half of A5

--- A5.py
+++ A5.py
@@ -6,6 +6,18 @@
 # Example:
 
 def highest_score(students):
+    best_score = None
+    for student in students:
+        if ((best_score is None) | (best_score < student['score'])):
+            best_score = student['score']
+            best_student = get_initials(student['name']) + str(student['id'])
+    return best_student
+
+def get_initials(name):
+    names = name.split(" ")
+    first_initial = names[0][0]
+    last_initial = names[1][0]
+    return first_initial + last_initial
 
 students = [
  {"name": 'Alvin Zablan', "id": 128, "score": -42},
@@ -14,7 +26,7 @@ students = [
  {"name": 'Fred Sladkey', "id": 256, "score": 94}
 ];
 
-print(highest_score(students)) #//=> 'TD2'
+# print(highest_score(students)) #//=> 'TD2'
 
 # Write a function snakeToCamel(str) that takes in a snake_cased string and returns
 # the string CamelCased. snake_case is a string where each word is separated with
@@ -24,11 +36,16 @@ print(highest_score(students)) #//=> 'TD2'
 # Examples:
 
 def snake_to_camel(str):
+    camel = ''
+    words = str.split("_")
+    for word in words:
+        camel += word[0].upper() + word[1:].lower()
+    return camel
 
-print(snake_to_camel('snakes_go_hiss')) # // => 'SnakesGoHiss'
-print(snake_to_camel('say_hello_world')) # // => 'SayHelloWorld'
-print(snake_to_camel('bootcamp_prep_is_cool')) # // => 'BootcampPrepIsCool'
-print(snake_to_camel('BOOtcamp_PREP_iS_cOol')) # // => 'BootcampPrepIsCool'
+# print(snake_to_camel('snakes_go_hiss')) # // => 'SnakesGoHiss'
+# print(snake_to_camel('say_hello_world')) # // => 'SayHelloWorld'
+# print(snake_to_camel('bootcamp_prep_is_cool')) # // => 'BootcampPrepIsCool'
+# print(snake_to_camel('BOOtcamp_PREP_iS_cOol')) # // => 'BootcampPrepIsCool'
 
 # Write a function sum2Darray(array) that takes in a 2-Dimensional array of numbers.
 # The function should return the total sum of all numbers in the 2D array.
@@ -36,13 +53,18 @@ print(snake_to_camel('BOOtcamp_PREP_iS_cOol')) # // => 'BootcampPrepIsCool'
 # Examples:
 
 def sum_2d_array(arr):
+    sum = 0
+    for subarr in arr:
+        sum += reduce(lambda x, y: x + y, subarr)
+    return sum
+
 
 arr1 = [
   [1,2,3],
   [4,5],
   [6],
 ];
-print(sum_2d_array(arr1)) # => 21
+# print(sum_2d_array(arr1)) # => 21
 
 arr2 = [
   [-10, 2, 3],
@@ -50,7 +72,7 @@ arr2 = [
   [10, -5],
   [2]
 ];
-print(sum_2d_array(arr2)) # => 3
+# print(sum_2d_array(arr2)) # => 3
 
 # Write a funtion minValueCallback(array, cb) that takes in an array of numbers and
 # a callback. The function should call `cb`, passing in the minimum number of the array.
@@ -59,18 +81,18 @@ print(sum_2d_array(arr2)) # => 3
 #
 # Examples:
 
-def min_value_callback(array, cb):
+# def min_value_callback(array, cb):
 
 # // Math.abs is a built in function to get the absolute value of anumber
-array1 = [-2, -7, 0, 8];
+# array1 = [-2, -7, 0, 8];
 # minValueCallback(array1, Math.abs); // => 7
 
 # // example cb
-def double(n):
-    return n * 2
-
-array2 = [12, 9, 20, 13, 14];
-print(min_value_callback(array2, double)) # => 18
+# def double(n):
+#     return n * 2
+#
+# array2 = [12, 9, 20, 13, 14];
+# print(min_value_callback(array2, double)) # => 18
 
 # Write a function mySelect(arr, cb) that accepts an array and a callback.
 # It should pass the callback every element, its corresponding index, and the array
@@ -79,16 +101,16 @@ print(min_value_callback(array2, double)) # => 18
 #
 # Examples:
 
-def my_select(arr, cb):
-
-def is_upper(str):
-    return str.upper() === str;
-
-def is_even(n):
-    return n % 2 === 0;
-
-result1 = my_select(['BOOTCAMP', 'prep', 'iS', 'COOL'], is_upper);
-result1 # => [ 'BOOTCAMP', 'COOL' ]
-
-result2 = my_select([1, 2, 4, 7, 8], is_even);
-result2 # => [ 2, 4, 8 ]
+# def my_select(arr, cb):
+#
+# def is_upper(str):
+#     return str.upper() === str;
+#
+# def is_even(n):
+#     return n % 2 === 0;
+#
+# result1 = my_select(['BOOTCAMP', 'prep', 'iS', 'COOL'], is_upper);
+# result1 # => [ 'BOOTCAMP', 'COOL' ]
+#
+# result2 = my_select([1, 2, 4, 7, 8], is_even);
+# result2 # => [ 2, 4, 8 ]
