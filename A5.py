97b74e9e308baf5e547fbf0498ce542fdc62d611
qydchen <qydchen@gmail.com>
2017-08-27 09:30:32 -0400

    complete A5

--- A5.py
+++ A5.py
@@ -1,10 +1,3 @@
-# Write a function highestScore(students) that takes in an array of student objects
-# as a parameter. It should return a string that corresponds to the student with
-# the highest score. The string should contain that student's initials
-# concatenated with their id. Assume the array contains at least 1 student object and
-# the student with the highest score is unique (there are no ties).
-# Example:
-
 def highest_score(students):
     best_score = None
     for student in students:
@@ -28,13 +21,6 @@ students = [
 
 # print(highest_score(students)) #//=> 'TD2'
 
-# Write a function snakeToCamel(str) that takes in a snake_cased string and returns
-# the string CamelCased. snake_case is a string where each word is separated with
-# underscores (_). CamelCase is a string where the first char of each word
-# is capitalized, all other characters lowercase.
-#
-# Examples:
-
 def snake_to_camel(str):
     camel = ''
     words = str.split("_")
@@ -47,17 +33,11 @@ def snake_to_camel(str):
 # print(snake_to_camel('bootcamp_prep_is_cool')) # // => 'BootcampPrepIsCool'
 # print(snake_to_camel('BOOtcamp_PREP_iS_cOol')) # // => 'BootcampPrepIsCool'
 
-# Write a function sum2Darray(array) that takes in a 2-Dimensional array of numbers.
-# The function should return the total sum of all numbers in the 2D array.
-#
-# Examples:
-
 def sum_2d_array(arr):
     sum = 0
     for subarr in arr:
         sum += reduce(lambda x, y: x + y, subarr)
     return sum
-
 
 arr1 = [
   [1,2,3],
@@ -74,43 +54,37 @@ arr2 = [
 ];
 # print(sum_2d_array(arr2)) # => 3
 
-# Write a funtion minValueCallback(array, cb) that takes in an array of numbers and
-# a callback. The function should call `cb`, passing in the minimum number of the array.
-# `minValueCallback` should return the return value of `cb`. If the array is empty,
-# then pass `null` into `cb`.
-#
-# Examples:
 
-# def min_value_callback(array, cb):
+def min_value_callback(array, cb):
+    return cb(min(array))
 
-# // Math.abs is a built in function to get the absolute value of anumber
-# array1 = [-2, -7, 0, 8];
-# minValueCallback(array1, Math.abs); // => 7
+
+# // abs is a built in function to get the absolute value of anumber
+array1 = [-2, -7, 0, 8]
+# print(min_value_callback(array1, abs)) # // => 7
 
 # // example cb
-# def double(n):
-#     return n * 2
-#
-# array2 = [12, 9, 20, 13, 14];
+def double(n):
+    return n * 2
+
+array2 = [12, 9, 20, 13, 14]
 # print(min_value_callback(array2, double)) # => 18
 
-# Write a function mySelect(arr, cb) that accepts an array and a callback.
-# It should pass the callback every element, its corresponding index, and the array
-# itself. Returns an array containing all elements of `arr` for which the given callback
-# returns a truthy value.
-#
-# Examples:
+def my_select(arr, cb):
+    result = []
+    for el in arr:
+        if cb(el):
+            result.append(el)
+    return result
 
-# def my_select(arr, cb):
-#
-# def is_upper(str):
-#     return str.upper() === str;
-#
-# def is_even(n):
-#     return n % 2 === 0;
-#
-# result1 = my_select(['BOOTCAMP', 'prep', 'iS', 'COOL'], is_upper);
-# result1 # => [ 'BOOTCAMP', 'COOL' ]
-#
-# result2 = my_select([1, 2, 4, 7, 8], is_even);
-# result2 # => [ 2, 4, 8 ]
+def is_upper(str):
+    return str.upper() == str;
+
+def is_even(n):
+    return n % 2 == 0;
+
+result1 = my_select(['BOOTCAMP', 'prep', 'iS', 'COOL'], is_upper);
+# print(result1) # => [ 'BOOTCAMP', 'COOL' ]
+
+result2 = my_select([1, 2, 4, 7, 8], is_even);
+# print(result2) # => [ 2, 4, 8 ]
