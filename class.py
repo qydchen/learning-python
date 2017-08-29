3943739dc454f087f4d942f9b921d6eafdb38d19
David Chen <qydchen@gmail.com>
2017-08-28 22:33:09 -0400

    linkedlist.

--- class.py
+++ class.py
@@ -1,12 +1,36 @@
-class Coordinate:
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
 
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+class LinkedList:
+    def __init__(self, *args):
+        self.head = self.create_list(*args)
 
+    def create_list(self, *args):
+        last_node = None
+        for nval in reversed(args):
+            current_node = Node(nval)
+            current_node.next = last_node
+            last_node = current_node
+        return last_node
 
-zero = Coordinate(0,0)
-oneone = Coordinate(1,1)
+    def print_ll(self):
+        self._print_ll(self.head)
 
-print(zero)
-print(oneone)
+    def _print_ll(self, node):
+        if node is None:
+            return
+        else:
+            print(node.val)
+            self._print_ll(node.next)
+
+    def iterative_print(self):
+        current_node = self.head
+        while current_node is not None:
+print(current_node.val)                                                                                             ``````````````````````````
+current_node = current_node.next             AQAQAAAÃA
+
+ll = LinkedList("a","b","c",'d','e')
+# ll.print_ll()
+ll.iterative_print()
