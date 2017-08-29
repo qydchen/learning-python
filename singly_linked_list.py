d4d143323b4f91be9dd8b14fa3d39aaf6b063943
David Chen <qydchen@gmail.com>
2017-08-28 23:10:41 -0400

    add pop and push methods

--- singly_linked_list.py
+++ singly_linked_list.py
@@ -25,12 +25,47 @@ class LinkedList:
             print(node.val)
             self._print_ll(node.next)
 
+    def reverse_print_ll(self):
+        self._reverse_print_ll(self.head)
+
+    def _reverse_print_ll(self, node):
+        if node is None:
+            return
+        else:
+            self._reverse_print_ll(node.next)
+            print(node.val)
+
     def iterative_print(self):
         current_node = self.head
         while current_node is not None:
-print(current_node.val)                                                                                             ``````````````````````````
-current_node = current_node.next             AQAQAAAÃA
+            print(current_node.val)
+            current_node = current_node.next
 
-ll = LinkedList("a","b","c",'d','e')
-# ll.print_ll()
-ll.iterative_print()
+    def pop(self):
+        if self.head.next is None:
+            self.head = None
+            return
+        else:
+            self.head = self.head.next
+            return self.head.val
+
+    def push(self, val):
+        if self.head is None:
+            self.head = Node(val)
+            return self.head.val
+
+        current_node = self.head
+
+        while current_node is not None:
+            if current_node.next is None:
+                current_node.next = Node(val)
+                return current_node.next.val
+            else:
+                current_node = current_node.next
+
+
+ll = LinkedList()
+ll.push("a")
+ll.print_ll()
+ll.push("b")
+ll.print_ll()
